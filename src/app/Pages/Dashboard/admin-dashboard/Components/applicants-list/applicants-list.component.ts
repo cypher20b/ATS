9a6bc8c49a88208821f323dabd4d6008b16b056e
amt-fidelis-mensah <fidelis.mensah@amalitech.org>
2024-03-27 17:32:40 +0000

    feat: added routing to the dashboard

---
+++ src/app/Pages/Dashboard/admin-dashboard/Components/applicants-list/applicants-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicants-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './applicants-list.component.html',
+  styleUrl: './applicants-list.component.scss'
+})
+export class ApplicantsListComponent {
+
+}
